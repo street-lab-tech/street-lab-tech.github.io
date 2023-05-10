1828fcc6a71d423ad777aab0ff17a6d3113ecc98
prch <4814073+prch@users.noreply.github.com>
2023-05-10 02:15:17 -0400

    Update site at 10 May 2023

---
+++ pushontogithub.bat
@@ -0,0 +1,25 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set repo=street-lab-tech/street-lab-tech.github.io
+set folder=street-lab
+set git=git
+set timestamp=%date:/=-%_%time::=-%
+
+echo "Building site..."
+jekyll build
+
+echo "Adding changes to Git..."
+%git% add --all
+
+echo "Committing changes to Git..."
+%git% commit -m "Update site at %timestamp%"
+
+echo "Pushing changes to GitHub..."
+%git% push
+
+echo "Site updated at %timestamp%"
+
+endlocal
+pause
